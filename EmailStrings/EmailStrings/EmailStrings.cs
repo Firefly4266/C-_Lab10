3186f13b67abf55ab360e13ebdd3eb0cb47c0053
Firefly4266 <gaines.johnp@gmail.com>
2017-08-22 22:53:41 -0700

    Part 2

--- EmailStrings/EmailStrings/EmailStrings.cs
+++ EmailStrings/EmailStrings/EmailStrings.cs
@@ -11,10 +11,34 @@ public class EmailStringe
         string email = iw.getString("email: ");
         while (! email.Equals(""))
         {
-            elements = email.Split(spaces);
-            foreach (string ele in elements)         
-                Console.WriteLine(ele);
-                email = iw.getString("email: ");
+            //elements = email.Split(spaces);
+            //foreach (string ele in elements)         
+            //    Console.WriteLine(ele);
+            //    email = iw.getString("email: ");
+            int index = email.IndexOf('@', 0);
+            if (index != -1)
+            {
+                string user = email.Substring(0, index);
+                int start = index + 1;
+                index = email.IndexOf('.', index);
+                if (index != -1)
+                {
+                    string domain = email.Substring(start, index - start);
+                    string suffix = email.Substring(index + 1);
+                    if (suffix == "com" || suffix == "org" || suffix == "edu")
+                    {
+                        Console.WriteLine("user = {0}", user);
+                        Console.WriteLine("domain = {0}", domain);
+                        Console.WriteLine("suffix = {0}", suffix);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Legal suffixes are .com, .org, .edu");
+                        break;
+                    }
+                }
+            }
         }
     }
 }

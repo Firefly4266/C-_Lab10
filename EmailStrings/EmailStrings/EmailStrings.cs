7f0730d6ccd528ea772f426e287150175ea7d234
Firefly4266 <gaines.johnp@gmail.com>
2017-08-22 22:28:58 -0700

    Part1

--- EmailStrings/EmailStrings/EmailStrings.cs
+++ EmailStrings/EmailStrings/EmailStrings.cs
@@ -4,6 +4,17 @@ public class EmailStringe
 {
     public static void Main()
     {
-
+        InputWrapper iw = new InputWrapper();
+        char[] spaces = { '@', '.' };
+        string[] elements;
+        Console.WriteLine("Enter a string, empty string to end");
+        string email = iw.getString("email: ");
+        while (! email.Equals(""))
+        {
+            elements = email.Split(spaces);
+            foreach (string ele in elements)         
+                Console.WriteLine(ele);
+                email = iw.getString("email: ");
+        }
     }
 }
